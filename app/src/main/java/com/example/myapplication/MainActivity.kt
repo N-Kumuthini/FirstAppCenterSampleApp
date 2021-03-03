0e706724f63b10fdf84fdd9a4c0b1dc5a9ca5923
AD\NatchiyappanK <kumuthini.natchiyappan@agilysys.com>
2021-03-03 20:40:37 +0530

    added analytics and crashlytics for app center

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCenter.start(
+            application, "0e784c5e-d9b3-4f41-a5af-4250c2a8677f",
+            Analytics::class.java, Crashes::class.java
+        )
+        Analytics.trackEvent("My custom event")
+        AppCenter.setLogLevel(Log.VERBOSE)
+
+//        throw IOException("Crash test")
     }
 }
